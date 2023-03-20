9b4a824567065e7f399e87e1c3c3f94b276cbf8a
Joko Nugroho <jocoding@gmail.com>
2023-03-20 19:00:58 +0700

    auth context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '@/components/AuthContext';
 import { Provider } from 'react-redux';
 import { store } from '@/services/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </Provider>
   )
 }
